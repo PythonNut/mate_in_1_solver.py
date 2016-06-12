c6844ca997c306bd8659e2ab3cfacf3f2de0a66c
PythonNut <PythonNut@users.noreply.github.com>
2016-06-12 03:32:24 +0000

    Add basic Simulated Annealing

--- python-mate-solver.py
+++ python-mate-solver.py
@@ -3,6 +3,7 @@ import math
 import random
 import time
 import itertools
+import decimal
 import multiprocessing as mp
 try:
     import numpy as np
@@ -324,5 +325,60 @@ def main():
         gen = create_new_generation(parents, gen_size)
         gen_number += 1
 
+def acc_prob(cur, new, temp):
+    if new > cur:
+        return 1
+    return math.exp((new - cur)/ temp)
+
+def join_boards(b1, b2):
+    b1 = str(b1).split('\n')
+    b2 = str(b2).split('\n')
+    res = []
+    for l1, l2 in zip(b1, b2):
+        res.append(l1 + " | " + l2)
+    return "\n".join(res)
+
+def main_sa():
+    temp = decimal.Decimal(20)
+    cooling_rate = decimal.Decimal(1) - decimal.Decimal('0.000001');
+    cur = generate_starting_board()
+    cur_fit = fitness(cur)[0]
+    best = cur
+    best_fit = cur_fit
+    count = 0
+    moves = 0
+    end = decimal.Decimal('0.1')
+    while temp > end:
+        while True:
+            new = mutate_board(cur)
+            if verify_board(new):
+                break
+        ft = fitness(new)
+        new_fit = ft[0]
+        moves += ft[1]
+        ap = acc_prob(cur_fit, new_fit, temp)
+        if ap > random.random():
+            cur = new
+            cur_fit = new_fit
+        if new_fit > best_fit:
+            best = new
+            best_fit = new_fit
+        count += 1
+        if count % 20 == 0:
+            print()
+            print(join_boards(cur, best))
+        print("{}/{:0>3}/{:0.6f}: {:0>3} {:0.3f}".format(
+            str(count).rjust(8, ' '),
+            best_fit,
+            temp,
+            cur_fit,
+            ap), end='\r')
+        temp *= cooling_rate
+
+    print("Best:")
+    print(best)
+    print("{} Mates".format(best_fit))
+
+
 if __name__ == "__main__":
-    main()
+    main_sa()
